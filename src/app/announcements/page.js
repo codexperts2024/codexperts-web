146a618e4c2f039246f84ea5d05d9aed6096a099
Tuan Khai Ngo <dannyngo263@gmail.com>
2026-04-24 11:17:03 -0400

    Add announment title

--- src/app/announcements/page.js
+++ src/app/announcements/page.js
@@ -1,9 +1,11 @@
 export default function AnnouncementsPage() {
   return (
-    <main className="min-h-screen flex items-center justify-center">
-      <div className="text-center">
-        <h1 className="text-5xl font-bold text-gray-900">Announcements</h1>
-        <p className="mt-3 text-gray-400 text-lg">Club updates · Important notices</p>
+    <main className="min-h-screen">
+      <div className="w-[45rem] p-10">
+        <p className="text-5xl font-semibold mb-4">Announcements</p>
+        <p className="text-gray-500">
+          The latest updates from the Digital Atelier. Tracking our progress,
+          architectural shifts, and upcoming academic deployments.</p>
       </div>
     </main>
   )
